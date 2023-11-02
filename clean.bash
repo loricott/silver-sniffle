59799f72b94fbfbcef10d76a70697a34cfd067f2
Lorenzo Ricotti <lor@pop-os.localdomain>
2023-11-02 11:05:10 +0100

    Added cleanup script.

---
+++ clean.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+CLIENT_BUILD_DIR=./build_client
+SERVER_BUILD_DIR=./build_server
+
+( cd ${CLIENT_BUILD_DIR} && make clean )
+( cd ${SERVER_BUILD_DIR} && make clean )
+
+/bin/rm -r ${CLIENT_BUILD_DIR}
+/bin/rm -r ${SERVER_BUILD_DIR}
