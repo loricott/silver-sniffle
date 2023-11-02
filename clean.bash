4983e38c83af05da83c7fa666ef4027d882d88af
Lorenzo Ricotti <lor@pop-os.localdomain>
2023-11-02 11:07:28 +0100

    Improved cleanup.

--- clean.bash
+++ clean.bash
@@ -5,5 +5,7 @@ SERVER_BUILD_DIR=./build_server
 ( cd ${CLIENT_BUILD_DIR} && make clean )
 ( cd ${SERVER_BUILD_DIR} && make clean )
 
+/bin/rm ./*.pem
+
 /bin/rm -r ${CLIENT_BUILD_DIR}
 /bin/rm -r ${SERVER_BUILD_DIR}
